67ab2a94ec5e7bd2e0ddf3d8db6a794e21ea7bbd
Yousuf Sarfaraz <yousufsarfaraz484@gmail.com>
2019-06-27 01:28:07 +0530

    Dynamic Content

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React ,{Component} from 'react';
 import './App.css';
 import Person from './Person/Person';
-function App() {
-  return <div class='App'>
-    <Person name ="Naman" age="21"/>
-    <Person name ="Mrinali" age="23">Hello</Person>
-    {React.createElement(Person ,{name:'Yousuf',age:'22'},null)}
-  </div>;
-}
+class App extends Component
+{
 
+  state={
+    persons:[
+      {name:'Naman',age:'21'},
+      {name:'Mrinali',age:'23'},
+      {name:'Yousuf',age:'22'}
+    ]
+  };
+  render() {
+
+    return (
+    <div class='App'>
+      <Person name ={this.state.persons[0].name} age={this.state.persons[0].age}/>
+      <Person name ={this.state.persons[1].name} age={this.state.persons[1].age}>Hello</Person>
+      {React.createElement(Person ,{name:this.state.persons[2].name,age:this.state.persons[2].age},null)}
+    </div>);
+  }
+}
 export default App;
