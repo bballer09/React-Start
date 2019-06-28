ab4d4168d710b518e85048e108d1f76b9930e4b1
Yousuf Sarfaraz <yousufsarfaraz484@gmail.com>
2019-06-29 00:53:46 +0530

    Exernal css

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import './Person.css'
 const Person = (props) => {
     return (
-        <div onClick={props.click}>
+        <div className="Person" onClick={props.click}>
             <p>I am a {props.name} and i am {props.age} years old!!</p>
             {
             props.children==null?
             <p style={{color: 'red'}}>No children</p>:<p>{props.children}</p>}
-            <input type="text" onChange={props.type}/>
+            <input type="text" value={props.name} onChange={props.type}/>
         </div>
     );
 };
