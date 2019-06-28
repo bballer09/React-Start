79dd8566dcb8c02dd1f14c445cf9bfe7300c5227
Yousuf Sarfaraz <yousufsarfaraz484@gmail.com>
2019-06-28 23:40:30 +0530

    State Change

--- src/App.js
+++ src/App.js
@@ -11,9 +11,20 @@ class App extends Component
       {name:'Yousuf',age:'22'}
     ]
   };
+
+  switchButtonClickHandler=()=>{
+    this.setState({
+      persons:[
+        {name:'Naman',age:(Number(this.state.persons[0].age)+1).toString()},
+        {name:'Mrinali',age:(Number(this.state.persons[1].age)+1).toString()},
+        {name:'Yousuf',age:(Number(this.state.persons[2].age)+1).toString()}]
+    });
+  }
+
   render() {
     return (
     <div class='App'>
+      <button onClick={this.switchButtonClickHandler}>Switch Name</button>
       <Person name ={this.state.persons[0].name} age={this.state.persons[0].age}/>
       <Person name ={this.state.persons[1].name} age={this.state.persons[1].age}>Hello</Person>
       {React.createElement(Person ,{name:this.state.persons[2].name,age:this.state.persons[2].age},null)}
