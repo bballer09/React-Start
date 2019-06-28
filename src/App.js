47b67dc1dd25481bc25bb91c434afe6944504b37
Yousuf Sarfaraz <yousufsarfaraz484@gmail.com>
2019-06-29 00:27:48 +0530

    sending value

--- src/App.js
+++ src/App.js
@@ -12,10 +12,10 @@ class App extends Component
     ]
   };
 
-  switchButtonClickHandler=()=>{
+  switchButtonClickHandler=(newName)=>{
     this.setState({
       persons:[
-        {name:'Naman',age:(Number(this.state.persons[0].age)+1).toString()},
+        {name:newName,age:(Number(this.state.persons[0].age)+1).toString()},
         {name:'Mrinali',age:(Number(this.state.persons[1].age)+1).toString()},
         {name:'Yousuf',age:(Number(this.state.persons[2].age)+1).toString()}]
     });
@@ -24,19 +24,19 @@ class App extends Component
   render() {
     return (
     <div class='App'>
-      <button onClick={this.switchButtonClickHandler}>Switch Name</button>
+      <button onClick={()=>this.switchButtonClickHandler('Naman')}>Switch Name</button>
       <Person 
       name ={this.state.persons[0].name} 
       age={this.state.persons[0].age}
-      click={this.switchButtonClickHandler}
+      click={() => this.switchButtonClickHandler('Chaman')}
       />
       <Person 
       name ={this.state.persons[1].name} 
       age={this.state.persons[1].age}
-      click={this.switchButtonClickHandler}
+      click={() => this.switchButtonClickHandler('Chaman')}
       >Hello</Person>
       {React.createElement(Person ,
-        {name:this.state.persons[2].name,age:this.state.persons[2].age,click:this.switchButtonClickHandler}
+        {name:this.state.persons[2].name,age:this.state.persons[2].age,click:this.switchButtonClickHandler.bind(this,'Chaman')}
       ,null)}
     </div>);
   }
